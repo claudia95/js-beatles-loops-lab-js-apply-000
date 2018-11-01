f361ff2d3dcdd793276e935720140f000db11c8a
Claudia Fox <claudiafox95@icloud.com>
2018-11-01 13:09:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(Musicians, Instruments) {
+  var array = [];
+  for (let i = 0; i < Musicians.length; i++) {
+    if (i === 0) {
+      array.push(`${Musicians[0]} plays ${Instruments[0]}`);
+    } else {
+      array.push(`${Musicians[i]} plays ${Instruments[i]}`);
+    }
+  } return array;
+}
+
+function johnLennonFacts(Facts) {
+  var i = 0;
+  while (i < Facts.length) {
+    Facts[i] = `${Facts[i]}!!!`;
+    i++;
+  } return Facts;
+}
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    number += 1;
+    array.push("I love the Beatles!");
+  } 
+  while (number < 15);
+  return array;
+}
